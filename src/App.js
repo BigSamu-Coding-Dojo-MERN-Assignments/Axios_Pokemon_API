e8ee573ccc4dabac34598589f4d6a8cda85d57a4
BigSamu <valdesgutierrez@gmail.com>
2020-09-17 19:37:56 -0300

    Submitting 'Axios Pokemon API' assignment

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+import './App.css';
+
+function App() {
+
+    const [pokemons, setPokemons] = useState({});
+    const [sendRequest, setSendRequest] = useState(false);
+
+    useEffect(() => {
+        if(sendRequest){
+            axios.get('https://pokeapi.co/api/v2/pokemon/?limit=1050')
+                .then(response => {
+                    setPokemons(response.data.results)
+                })
+                .catch()
+            setSendRequest(false);
+        }
+    }, [sendRequest]);
+
+    const onClick = (e) => {
+        setSendRequest(true)
+    }
+
+    return (
+        <div className="App bg-light p-3 text-left">
+        <div className="w-25 mt-2 mx-auto">
+            <button className="btn btn-secondary mb-3 d-block" onClick={onClick}> Fetch Pokemon</button>
+            <p className="mb-1">List of Pokemons: </p>
+            <ol>
+                {pokemons.length > 0 && pokemons.map((pokemon, index)=>
+                    <li key={index}>{pokemon.name}</li>
+                )}
+            </ol>
+        </div>
+    </div>
+    );
+}
+
+export default App;
